e856b3fb84e59a6d43f267411fcf4e69fe296912
kirill_fedorenko <kurull.fedorenko@gmail.com>
2022-12-27 15:43:01 +0200

    Change description

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 
-$(document).ready(function(){
-        
+$(document).ready(function () {
 
 
-    // MODAL
-    $('[data-modal=projects]').on('click', function(){
-        $('.overlay, #projects').fadeIn('fast');
-    });
-    $('.modal__close').on('click', function(){
-        $('.overlay, #projects').fadeOut('fast');
-    })
+	// MODAL
+	$('[data-modal=projects]').on('click', function () {
+		$('.overlay, #projects').fadeIn('fast');
+	});
+	$('[data-modal=social]').on('click', function () {
+		$('.overlay, #social').fadeIn('fast');
+	});
+	// $('[data-modal=projects]').on('click', function () {
+	// 	$('#social').fadeOut('fast');
+	// });
+	$('.modal__close').on('click', function () {
+		$('.overlay, #projects, #social').fadeOut('fast');
+	})
+	$('.overlay').on('click', function () {
+		$('.overlay, #projects, #social').fadeOut('fast');
+	})
+	$(document).on('keydown', function (event) {
+		if (event.key == "Escape") {
+			$('.overlay, #projects, #social').fadeOut('fast');;
+		}
+	});
 });
